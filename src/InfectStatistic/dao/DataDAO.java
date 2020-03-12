3f1ca8fc36afed8ba299a83ad73326e03eddb926
hujh4779 <823362797@qq.com>
2020-03-12 14:29:44 +0800

    [HJH]&（Update DataDao.java and DataServlet.java）
    补充servlet

--- src/InfectStatistic/dao/DataDAO.java
+++ src/InfectStatistic/dao/DataDAO.java
@@ -10,7 +10,7 @@ import java.sql.Statement;
 
 public class DataDAO {
 
-    public static JSONArray getTotalData(String endDate) {
+    public JSONArray getTotalData(String endDate) {
         String[] provinceString = {"全国", "安徽", "北京", "重庆", "福建", "甘肃", "广东", "广西", "贵州", "海南",
                 "河北", "河南", "黑龙江", "湖北", "湖南", "吉林", "江苏", "江西", "辽宁", "内蒙古", "宁夏", "青海", "山东",
                 "山西", "陕西", "上海", "四川", "天津", "西藏", "新疆", "云南", "浙江", "台湾", "香港", "澳门"};
@@ -58,7 +58,7 @@ public class DataDAO {
         return jsonArray;
     }
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         System.out.println(getTotalData("2020-02-01"));
-    }
+    }*/
 }
--- src/InfectStatistic/servlet/DataServlet.java
+++ src/InfectStatistic/servlet/DataServlet.java
@@ -1,5 +1,7 @@
 package InfectStatistic.servlet;
 
+import InfectStatistic.dao.DataDAO;
+import net.sf.json.JSONArray;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,10 +14,29 @@ public class DataServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        String action = request.getParameter("action");
+        if (action != null && action.equals("getTotalData")) {
+            this.getTotalData(request, response);
+        } else if (action != null && action.equals("getDailyData")) {
 
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
+    }
+
+    private void getTotalData(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        DataDAO dataDAO = new DataDAO();
+        String endDate = request.getParameter("endDate");
+        if (endDate != null) {
+        } else {
+            endDate = "2020-02-02";
+        }
+        JSONArray jsonArray = dataDAO.getTotalData(endDate);
+        request.setAttribute("totalData", jsonArray);
+        request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 }
