6bdbc30b6cd03119b4c4293e6c9a5e77cd79546b
hujh4779 <823362797@qq.com>
2020-03-12 11:26:42 +0800

    [HJH]&（Create Data.java & create DataDAO.java & create DataServlet.java）
    创建Data，DataDAO，DataServlet模板

---
+++ src/InfectStatistic/dao/DataDAO.java
@@ -0,0 +1,14 @@
+package InfectStatistic.dao;
+
+import InfectStatistic.entity.Data;
+import InfectStatistic.util.DBConnect;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DataDAO {
+    
+}
